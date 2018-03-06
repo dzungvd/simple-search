e39706dc8c9c24bfae37c88d731dc8ccea3238f3
hthinh <thinh.nh@anlab.info>
2018-03-06 09:55:35 +0700

    remove deserialize type in keyword query mes

--- file_server/src/message.cpp
+++ file_server/src/message.cpp
@@ -12,18 +12,9 @@ namespace bitmile {
       /*
        * Parse binary data
        */
-      if (size < sizeof (MessageType)) {
-        return;
-      }
 
-      MessageType type;
-      memcpy (&type, dat, sizeof (MessageType));
+      int offset = 0;
 
-      dat += sizeof (MessageType);
-
-      int offset = sizeof (MessageType);
-
-      int keyword_len = 0;
 
       if (size <= 1) {
         //invalid string since one string must contain
@@ -32,13 +23,14 @@ namespace bitmile {
         return;
       }
 
-      if (dat[size - 1 - sizeof (MessageType)] != '\0') {
+      if (dat[size] != '\0') {
         //the last character should be a terminate NULL
         //or else it's an invalid message
         return;
       }
 
       //TODO: handle case "continuous NULL character"
+      int keyword_len = 0;
       while (offset < size) {
         keywords_.push_back (std::string (dat));
         keyword_len = keywords_.back().length() + 1;
@@ -91,11 +83,50 @@ namespace bitmile {
     }
 
     void KeywordQueryReplyMes::Serialize (std::vector<char>& return_data) {
-      return_data.resize (sizeof (MessageType));
-      memcpy (return_data.data(), &type_, sizeof (MessageType));
+      /*
+       * reply query mes:
+       * ----------------------------------------
+       *|type|owner_address_1|doc_id_1|elastic_doc_id_1|....
+       * ----------------------------------------
+       * type - int - 4 bytes
+       * owner_address - string - owner blockchain address
+       * doc_id - string - user document id in user database
+       * elastic_doc_id - string - elastic system doc id
+       * strings are seperated by NULL char
+       */
+
+      int size = sizeof (MessageType);
+      for (int i = 0; i < docs_.size() ; i++) {
+        size += docs_[i].GetOwnerAddress().length() + 1;
+        size += docs_[i].GetOwnerDocId().length() + 1;
+        size += docs_[i].GetElasticDocId().length() + 1;
+      }
+
+      return_data.resize (size);
+      int offset = 0;
+      memcpy (&return_data[offset], &type_, sizeof (MessageType));
+
+      std::string owner_addr, owner_doc_id, elastic_id;
+      for (int i = 0; i < docs_.size() ; i++) {
+        owner_addr = docs_[i].GetOwnerAddress();
+        owner_doc_id = docs_[i].GetOwnerDocId();
+        elastic_id = docs_[i].GetElasticDocId();
+
+        memcpy (&return_data[offset], owner_addr.c_str(), owner_addr.length() + 1);
+        offset += owner_addr.length() + 1;
+        memcpy (&return_data[offset], owner_doc_id.c_str(), owner_doc_id.length() + 1);
+        offset += owner_doc_id.length() + 1;
+        memcpy (&return_data[offset], elastic_id.c_str(), elastic_id.length() + 1);
+        offset += elastic_id.length() + 1;
+      }
+
     }
     void KeywordQueryReplyMes::Deserialize (const char* dat, size_t size) {
 
+    }
+
+    void KeywordQueryReplyMes::SetDocuments (std::vector<db::Document>& input_doc) {
+      docs_ = std::vector <db::Document> (input_doc);
     }
     ErrorMes::ErrorMes (MessageType type, char* dat, size_t size){
       type_ = type;
