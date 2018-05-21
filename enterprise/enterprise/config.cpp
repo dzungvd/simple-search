fbedb79e2dceed5f55fd06eb51e1f4a3fb47f4e1
Nguyen Trong Thang <thang.nt@anlab.pro>
2018-05-21 10:22:08 +0700

    add space on config.cpp

--- enterprise/enterprise/config.cpp
+++ enterprise/enterprise/config.cpp
@@ -4,7 +4,7 @@ Config::Config()
 {
     deal_contract_address_ = "0xb6b29ef90120bec597939e0eda6b8a9164f75deb";
     user_key_contract_address_ = "0x70f94d58cc3fdcbeac7140f35a087da9fcd09b94";
-    wallet_address_ = "0xbf011be8b2cee69ef4bd242013377b8945298dde";
+     wallet_address_ = "0xbf011be8b2cee69ef4bd242013377b8945298dde";
 }
 
 Config* Config::getInstance() {
